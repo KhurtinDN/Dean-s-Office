ae03cf70a36bdfb0b5ac9d53ecf16e72900f0ec2
hd <KhurtinDN@gmail.com>
2010-10-08 13:10:51 +0400

    Добавил файлы, забытые в прошлый раз

---
+++ web/src/main/java/ru/sgu/csit/inoc/deansoffice/client/gxt/content/StudentGrid.java
@@ -0,0 +1,54 @@
+package ru.sgu.csit.inoc.deansoffice.client.gxt.content;
+
+import com.extjs.gxt.ui.client.binding.FormBinding;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
+import ru.sgu.csit.inoc.deansoffice.shared.dto.StudentDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: hd (KhurtinDN(a)gmail.com)
+ * Date: Oct 7, 2010
+ * Time: 11:53:33 PM
+ */
+public class StudentGrid extends Grid<StudentDto> {
+    private static ColumnModel createGridColumnModel() {
+        List<ColumnConfig> columnConfigList = new ArrayList<ColumnConfig>();
+        columnConfigList.add(new ColumnConfig("nn", "N", 40));
+        columnConfigList.add(new ColumnConfig("name", "Имя", 500));
+        columnConfigList.add(new ColumnConfig("studyForm", "Форма обучения", 100));
+
+        return new ColumnModel(columnConfigList);
+    }
+
+    static ColumnModel columnModel = createGridColumnModel();
+
+    protected StudentGrid() {
+        super(new ListStore<StudentDto>(), columnModel);
+    }
+
+    public void update(String groupName, List<StudentDto> studentDtoList) {
+        ListStore<StudentDto> gridListStore = createStudentGridListStore(groupName, studentDtoList);
+        reconfigure(gridListStore, columnModel);
+    }
+
+    private ListStore<StudentDto> createStudentGridListStore(String groupName, List<StudentDto> studentDtoList) {
+        ListStore<StudentDto> listStore = new ListStore<StudentDto>();
+
+        Long nn = 1L;
+        for (StudentDto studentDto : studentDtoList) {
+            studentDto.set("nn", nn++);
+            studentDto.set("groupName", groupName);
+            listStore.add(studentDto);
+        }
+
+        return listStore;
+    }
+}
