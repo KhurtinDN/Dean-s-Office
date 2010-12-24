e479d6dca555acf6decc4d59ea356bb7fde1a80f
XX <freecoder.xx@gmail.com>
2010-12-24 13:25:10 +0300

    Доработан класс Photo.

--- common/src/main/java/ru/sgu/csit/inoc/deansoffice/domain/Photo.java
+++ common/src/main/java/ru/sgu/csit/inoc/deansoffice/domain/Photo.java
@@ -14,6 +14,22 @@ public class Photo extends PersistentItem {
     @Lob
     private byte[] data;
 
+    public String getFormat() {
+        String header = new String(data, 0, 10).trim();
+
+        if (header.contains("PNG")) {
+            return "png";
+        } else if (header.contains("GIF89")) {
+            return "gif";
+        } else if (header.contains("BM")) {
+            return "bmp";
+        } else if (header.contains("JFIF")) {
+            return "jpg";
+        } else {
+            return "wbmp";
+        }
+    }
+
     public byte[] getData() {
         return data;
     }
