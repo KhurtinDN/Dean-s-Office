6408db388c39187f20209a538ad5773e096e36ae
Khurtin Denis <KhurtinDN@gmail.com>
2011-01-26 13:36:45 +0300

    Создан новый модуль GWT (  в ru.sgu.csit.inoc.deansoffice.webui.gxt.students ), в котором будет создаваться модуль системы управления студентами (пока только справки и учетная карточка студента)

---
+++ web/src/main/java/ru/sgu/csit/inoc/deansoffice/webui/gxt/students/client/Students.java
@@ -0,0 +1,14 @@
+package ru.sgu.csit.inoc.deansoffice.webui.gxt.students.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+/**
+ * User: Khurtin Denis (KhurtinDN@gmail.com)
+ * Date: 1/26/11
+ * Time: 1:00 PM
+ */
+public class Students implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}
