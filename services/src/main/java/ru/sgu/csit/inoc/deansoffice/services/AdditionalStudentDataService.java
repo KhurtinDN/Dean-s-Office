5a45081464cf3e8aa54615154ca568cf1e040160
XX <freecoder.xx@gmail.com>
2011-02-17 15:09:14 +0300

    Возня с паспортами.

---
+++ services/src/main/java/ru/sgu/csit/inoc/deansoffice/services/AdditionalStudentDataService.java
@@ -0,0 +1,13 @@
+package ru.sgu.csit.inoc.deansoffice.services;
+
+import ru.sgu.csit.inoc.deansoffice.domain.Passport;
+
+import java.util.List;
+
+/**
+ * User: XX (freecoder.xx@gmail.com)
+ * Date: 17.02.11
+ * Time: 14:46
+ */
+public interface AdditionalStudentDataService {
+}
---
+++ services/src/main/java/ru/sgu/csit/inoc/deansoffice/services/impl/AdditionalStudentDataServiceImpl.java
@@ -0,0 +1,14 @@
+package ru.sgu.csit.inoc.deansoffice.services.impl;
+
+import ru.sgu.csit.inoc.deansoffice.domain.Passport;
+import ru.sgu.csit.inoc.deansoffice.services.AdditionalStudentDataService;
+
+import java.util.List;
+
+/**
+ * User: XX (freecoder.xx@gmail.com)
+ * Date: 17.02.11
+ * Time: 14:47
+ */
+public class AdditionalStudentDataServiceImpl implements AdditionalStudentDataService {
+}
